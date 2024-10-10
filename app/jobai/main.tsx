671af2a2ac4c2768ea6baacbd7ca01772df6c198
Marcus R. Brown <git@mrbro.dev>
2024-10-10 06:42:36 -0700

    Restore other resume upload control (#20)

--- app/jobai/main.tsx
+++ app/jobai/main.tsx
@@ -1,23 +1,43 @@
 'use client'
 
-import React from 'react'
+import {useState} from 'react'
 
+import {useAssistant} from '@/lib/hooks/use-assistant'
+import ChatInterface from '@/app/jobai/components/chat-interface'
 import FeaturesSection from '@/app/jobai/components/features-section'
 import Footer from '@/app/jobai/components/footer'
 import Header from '@/app/jobai/components/header'
 import HeroSection from '@/app/jobai/components/hero-section'
 import HowItWorksSection from '@/app/jobai/components/how-it-works-section'
 import ProfileImageSection from '@/app/jobai/components/profile-image-section'
-import ResumeAnalyzer from '@/app/jobai/components/resume-analyzer'
+import ResumeUploader from '@/app/jobai/components/resume-uploader'
+
+const assistantId = process.env.NEXT_PUBLIC_OPENAI_ASSISTANT_ID
 
 export default function JobAssistantChatbot() {
+  const [message, setMessage] = useState('')
+  const {isLoading, messages, sendMessage, threadId} = useAssistant(assistantId!)
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    // Handle message submission
+    sendMessage(message)
+  }
+
+  // TODO: Do something with isLoading (i.e. show a toast or spinner)
+  // TODO: Do something with messages (i.e. display in chat interface)
+  // TODO: Wire up handleSubmit to submitMessage
+
   return (
     <div className="min-h-screen flex flex-col bg-background">
       <Header />
       <HeroSection />
       <main className="flex-grow w-full bg-background py-12">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <ResumeAnalyzer />
+          <div className="flex flex-col lg:flex-row gap-8">
+            <ChatInterface message={message} setMessage={setMessage} handleSubmit={handleSubmit} />
+            <ResumeUploader threadId={threadId} />
+          </div>
         </div>
       </main>
       <ProfileImageSection />
